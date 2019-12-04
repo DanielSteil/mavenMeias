091123a5bf2d9a113ba6884d7b19daad355a2acb
Amauri Hoesel <amaurirednose@hotmail.com>
2019-12-04 15:21:14 -0200

    Paginacao

--- src/main/java/br/com/meias/apirest/service/MeiaService.java
+++ src/main/java/br/com/meias/apirest/service/MeiaService.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import br.com.meias.apirest.dto.MeiaDTO;
 import br.com.meias.apirest.model.Meia;
 import br.com.meias.apirest.repository.MeiaRepository;
 
@@ -30,5 +33,10 @@ public class MeiaService {
 	
 	public Meia update(Meia meia) {
 		return mr.save(meia);
+	}
+	
+	public Page<MeiaDTO> consultaFiltro(MeiaDTO filter, int pageIndex, int pageSize, String authorization){
+		return mr.consultaFiltro(filter, PageRequest.of(pageIndex, pageSize)).map(MeiaDTO::fromEntity);
+		
 	}
 }
